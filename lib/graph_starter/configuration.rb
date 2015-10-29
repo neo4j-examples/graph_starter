ff984f2dd6c9690739ab65efc459a084eb97aaf2
Brian Underwood <ml+github@semi-sentient.com>
2015-10-29 17:41:57 +0800

    Add some configuration options and validate options when they are set

--- lib/graph_starter/configuration.rb
+++ lib/graph_starter/configuration.rb
@@ -2,8 +2,26 @@ module GraphStarter
   class Configuration
     attr_writer :user_class
 
+    attr_accessor :menu_models, :icon_classes
+
+    def initialize
+      @icon_classes = {}
+    end
+
     def user_class
       @user_class || (:User if defined?(::User))
+    end
+
+    def validation_errors
+      {}.tap do |errors|
+        if !(@menu_models.respond_to?(:each) || @menu_models.nil?)
+          errors[:menu_models] = 'should be enumerable or nil'
+        end
+
+        if !@icon_classes.is_a?(Hash)
+          errors[:icon_classes] = 'should be a Hash'
+        end
+      end
     end
   end
 end
@@ -13,12 +31,16 @@ module GraphStarter
 
   def self.configure(config_hash = nil)
     if config_hash.nil?
-      yield Configuration.new
+      yield CONFIG
     else
       config_hash.each do |key, value|
         CONFIG.send("#{key}=", value)
       end
     end
+
+    errors = CONFIG.validation_errors
+
+    fail "GraphStarter validation errors: #{errors.inspect}" if errors.size > 0
   end
 
   def self.configuration
